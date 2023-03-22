3a19455931a85bdc6d2c66d849c1a2265a050fe1
Killian Monnier <mauxnier@gmail.com>
2023-03-22 09:59:27 +0100

    add code

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -6,24 +6,56 @@
 import Vqs._
 import Vqs.operators._
 
-// lancer le programme avec la commande "sbt compile" puis "sbt run" dans le dossier VisuelQuantumSimulator
 object main {
-   // Permet de simuler la transmission de la valeur vcode
-   def superDense(vcode : Int) {
-      println("Alice (#1) and Bob (#2) share an EPR pair");
-      println("Alice encode the value 0");
-      println("Alice send its QBit to Bob");
-      println("Bob decode the QBit from Alice");
-      println("Bob perform a measurement on the QBit");
-      println("Bob read 0");
+      // lancer le programme avec la commande "sbt compile" puis "sbt run" dans le dossier VisuelQuantumSimulator
+      def main(args: Array[String]): Unit = {
+            QReg.setDefaultUseColor()
+            QReg.setDefaultUseASCII()
 
-      rr - $(0, "Bob") - $(1, "Alice")
-      rr - Label("Alice -> Bob")
-      rr - C(Not(0), 1)
-   }
-   
-   def main(args : Array[String]): Unit = {
-      println("Run");
-      superDense(0);
-   }
+            println("Super Dense Coding")
+
+            println("vcode = 0 : ")
+            superDense(0)
+
+            println("vcode = 1 : ")
+            superDense(1)
+
+            println("vcode = 2 : ")
+            superDense(2)
+
+            println("vcode = 3 : ")
+            superDense(3)
+      }
+
+      // Permet de simuler la transmission de la valeur vcode
+      def superDense(vcode : Int) { 
+            val rr : QReg = QReg(2);
+
+            rr - $(0, "Bob") - $(1, "Alice")
+
+            rr - H(0)
+            rr - C(Not(1), 0)
+
+            vcode match {
+                  case 1 => rr - X(1)
+                  case 2 => rr - Z(1)
+                  case 3 => rr - Y(1)
+                  case _ => ()
+            }
+
+            rr - Label("Alice -> Bob")
+
+            rr - C(Not(0), 1)
+            rr - H(1)
+
+            rr - <(0)
+            rr - <(1)
+
+            println(rr.render)
+
+            println("Bob measure : " + rr.?(0))
+            println("Alice measure : " + rr.?(1))
+
+            rr.end
+      }
 }
